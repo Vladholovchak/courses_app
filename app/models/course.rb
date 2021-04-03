b76aef3b727c1d1ff4b5cfceaf978ef36a68b3fa
Vladholovchak <vlad.golovchak.1996@gmail.com>
2021-04-03 09:51:37 +0300

    rubocop style refactoring

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,4 @@
 class Course < ApplicationRecord
-
   validates :title, :description, presence: true
   has_rich_text :description
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 30.times do
   Course.create!([{
-    title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
-      }])
+                   title: Faker::Educator.course_name,
+                   description: Faker::TvShows::GameOfThrones.quote
+                 }])
 end
