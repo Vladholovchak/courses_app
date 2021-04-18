a692c5be0091196b093e55dcf24fc19331cdf2fc
Vladholovchak <vlad.golovchak.1996@gmail.com>
2021-04-19 00:50:42 +0300

    add trackeble to users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     @courses = Course.all.limit(3)
-    @latest_courses = Course.all.all.limit(3).order(created_at: :desc)
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 
   def privacy_policy; end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
+  resources :users
   root 'home#index'
   get 'privacy_policy', to: 'home#privacy_policy'
 end
